598d69456b3951c279744376d1e7d1b9295a3522
Esteban Castillo <steban.94@hotmail.com>
2017-09-20 12:47:52 -0600

    Implemented class ServerHTTP and parser for HTTP request

---
+++ HTTPParser.py
@@ -0,0 +1,35 @@
+# Parser de HTTP. Crea un diccionario con el método, la versión de HTTP, la URI
+# y los headers del resto del request
+def parse(strRequest):
+	requestParts = strRequest.split('\r\n')
+	requestFirstLine = requestParts[0]
+
+	# Separa la primera línea del request para obtener METHOD, HTTPVERSION y URI 
+	def parseRequetsLine(requestLine):
+		requestLineParts = requestLine.split(" ")
+		requestDic = 	{"METHOD": requestLineParts[0],
+						 "HTTPVERSION": requestLineParts[2]}
+		if requestLineParts[1].find("?") == -1:
+			requestDic.update({"URI":requestLineParts[1]})
+		else:
+			uri = requestLineParts[1].split("?")
+			requestDic.update({ "URI":uri[0],
+						   "DATA":uri[1]})
+		return requestDic
+
+	#Separa el resto del request en encabezados y su valor.
+	def parseHeaders(requestDic, requestParts):
+		for i in range (1, len(requestParts)-2):
+			headerParts = requestParts[i].split(": ")
+			requestDic.update({headerParts[0]: headerParts[1]})
+		return requestDic
+
+	requestDictionary = parseRequetsLine(requestFirstLine)
+	requestDictionary = parseHeaders(requestDictionary, requestParts)
+	return requestDictionary
+
+
+
+#parser = HTTPPaser()
+#dic = parser.parse(str)
+#print(dic)
---
+++ ServerHTTP.py
@@ -0,0 +1,70 @@
+#Imports--------------------------
+import socket
+import signal
+import time
+import sys
+#import HTTPMethodHandler
+#import HTTPResponse
+from HTTPParser import parse
+#---------------------------------
+
+PORT = 8000 # Puerto por el cual escucha el servidor.
+			# Se puede modificar por si se desea escuchar en otro puerto.
+
+class ServerHTTP:
+
+	#Contructor de  la clase
+	def __init__(self):
+		self.host = ''
+		self.port = PORT
+		self.my_dir = (self.host, self.port)
+		#self.www_dir = 'www'
+
+	#Levanta el servidor
+	def serve(self):
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			print("Tratando de inicializar en el puerto:", self.port, "...")
+			self.socket.bind(self.my_dir)
+
+		except Exception as e:
+			print("No se pudo establecer conexión con el puerto:", self.port)
+			self.stop()
+			sys.exit(1)
+
+		print("Conexión exitosa con el puerto:", self.port, "!")
+		self.esperar_conexion()
+
+	#Baja el servidor
+	def stop(self):
+		try:
+			print("Apagando el servidor...")
+			sys.exit(1)
+
+		except Exception as e:
+	         print("No se puede cerrar conexión con el socket",e)
+
+	def esperar_conexion(self):
+		while True:
+			self.socket.listen(3)
+			conn, addr = self.socket.accept()
+
+			print("Got connection from:", addr)
+			data = conn.recv(1024) #receive data from client
+			request = bytes.decode(data) #decode it to string
+
+			dictionary = parse(request)
+			print(dictionary)
+
+
+def graceful_shutdown(sig, dummy):
+	server.stop() #shut down the server
+	sys.exit(1)
+
+
+print ("Inicializando servidor...")
+server = ServerHTTP()  		# construct server object
+server.serve() 	# aquire the socket
+
+
+
